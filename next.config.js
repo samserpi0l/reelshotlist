b4347b9ade6948f08efa4f9844e1106a3106d72b
samserpi0l <samserpi0l@proton.me>
2025-08-13 14:45:10 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,2 +1,15 @@
-const nextConfig = { reactStrictMode: true };
-module.exports = nextConfig;
+// next.config.js
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  register: true,
+  skipWaiting: true,
+  runtimeCaching: require('next-pwa/cache'),
+  fallbacks: {
+    document: '/_offline', // unsere Offline-Seite
+  },
+});
+
+module.exports = withPWA({
+  reactStrictMode: true,
+});
